609fa66729759c2bb557873b2e2791588f4adc6e
Xu Aikun <727850024@qq.com>
2019-01-08 13:16:21 +0800

    Aikun_Xu

--- 201810_201909_BeforeGraduateStudent/python_programming/Period_Charge/TourConstructionProblem.py
+++ 201810_201909_BeforeGraduateStudent/python_programming/Period_Charge/TourConstructionProblem.py
@@ -14,7 +14,7 @@ import time
 import math
 from matplotlib.ticker import MultipleLocator
 # 数据初始化
-N = 6   # 假设我有N辆电单车
+N = 10   # 假设我有N辆电单车
 edge_n = 500 # 假设定义的二维空间范围是 edge_n * edge_n
 
 # 初始化电单车在二维空间中的坐标
@@ -58,14 +58,17 @@ def select_sort(array):
 
 # 分别存放所有点的横坐标和纵坐标，一一对应
 # 将图中离散的点一一连接
-obstacle_coordinate = np.empty([2, 1], list)
-obstacle_coordinate[0][0] = [30,400,333,111,444]
-obstacle_coordinate[1][0] = [50,100,400,322,123]
+obstacle_coordinate = []
+obstacles_Num =20
+x_down = np.empty([1, obstacles_Num], int)
+x_up = np.empty([1, obstacles_Num], int)
+y_down = np.empty([1, obstacles_Num], int)
+y_up = np.empty([1, obstacles_Num], int)
+
 def AllNodeLink(x, y, obstacle_coordinate_new):
     x_list = x
     y_list = y
-    x_obstacle_list = obstacle_coordinate_new[0][0]
-    y_obstacle_list = obstacle_coordinate_new[1][0]
+    
     # 调整生成的图片的大小
     plt.rcParams['figure.figsize'] = (10.0, 10.0) # 设置figure_size尺寸
     #创建图并命名
@@ -74,9 +77,30 @@ def AllNodeLink(x, y, obstacle_coordinate_new):
     ax = plt.gca()
     
     plt.grid()
+    # 障碍坐标区域使用绿色表示
+    # 障碍横坐标x的范围
+    x_down_list = obstacle_coordinate_new[0]
+    x_up_list  = obstacle_coordinate_new[1]
+
+    # 障碍纵坐标y的范围
+    y_down_list = obstacle_coordinate_new[2]
+    y_up_list = obstacle_coordinate_new[3]
+    for j in range(0, obstacles_Num):
+        x_down_value= x_down_list[0][j]
+        x_up_value= x_up_list[0][j]
+        y_down_value= y_down_list[0][j]
+        y_up_value= y_up_list[0][j]
+       
+        plt.fill_between(range(x_down_value, x_up_value), y_down_value , y_up_value, facecolor='green')
+    ax.scatter((x_down_list[0][1]+x_up_list[0][1])/2, (
+             y_down_list[0][1]+y_up_list[0][1])/2, s = 100, color = 'green',label = 'Obstacle', marker = 's')
     # 图片坐标刻度设置
-    ax.xaxis.set_major_locator(MultipleLocator(10))
-    ax.yaxis.set_major_locator(MultipleLocator(10))
+    # 2000*2000
+    # ax.xaxis.set_major_locator(MultipleLocator(100))
+    # ax.yaxis.set_major_locator(MultipleLocator(40))
+    
+    ax.xaxis.set_major_locator(MultipleLocator(40))
+    ax.yaxis.set_major_locator(MultipleLocator(20))
     #设置x轴、y轴名称
     ax.set_xlabel('X(m)')
     ax.set_ylabel('Y(m)')
@@ -84,29 +108,32 @@ def AllNodeLink(x, y, obstacle_coordinate_new):
     print "x_list =", x_list
     print "y_list =", y_list
     # 每个节点用红圈圈表示出来
-    ax.scatter(x_list, y_list,color = 'r', marker = 'o')
-    ax.scatter(x_obstacle_list, y_obstacle_list,color = 'g', marker = '8')
+    ax.scatter(x_list, y_list,color = 'r',label = 'Node', marker = 'o')
+    # ax.scatter(x_obstacle_list, y_obstacle_list,color = 'g', marker = '8')
     # S点为红色正方形，并且大一点
-    ax.scatter(x_list[0], y_list[0], s = 100, color = 'k', marker = 's')
+    ax.scatter(x_list[0], y_list[0], s = 100, color = 'k',label = 'S', marker = 's')
  
     ax.text(x_list[0], y_list[0], 'S', fontsize=20)
     for i in range(1, len(y_list)):
         ax.text(x_list[i], y_list[i], i, fontsize = 10)
     # 将所有节点与充电桩S连起来
-    for j in range(1, len(y_list)):
+    # for j in range(1, len(y_list)):
         # 两两连接
-        x_temp = [x_list[0], x_list[j]]
-        y_temp = [y_list[0], y_list[j]]
+        # x_temp = [x_list[0], x_list[j]]
+        # y_temp = [y_list[0], y_list[j]]
         # 调节途中线条颜色，粗细
-        plt.plot(x_temp, y_temp, 'b',linewidth=0.5)
-            
+        # plt.plot(x_temp, y_temp, 'b',linewidth=0.5)
+    ax.legend(loc='best', edgecolor='black')
+    # plt.legend(loc='best',frameon=False) #去掉图例边框
+    # plt.legend(loc='best',edgecolor='blue') #设置图例边框颜色
+    # plt.legend(loc='best',facecolor='blue') #设置图例背景颜色,若无边框,参数无效
     plt.xlim([0 - 1,edge_n + 1]) #设置绘图X边界                                                                                                   
     plt.ylim([0 - 1,edge_n + 1]) #设置绘图Y边界
     plt.show()
     return
 
 # 将节点前后连接起来
-def NodeToOtherNodeLink(x, y, label):
+def NodeToOtherNodeLink(x, y, label, obstacle_coordinate_new):
     print "x =", x
     print "y =", y
     #分别存放所有点的横坐标和纵坐标，一一对应
@@ -118,19 +145,41 @@ def NodeToOtherNodeLink(x, y, label):
     plt.title('S & Node')
     ax = plt.gca()
     plt.grid()
-     # 图片坐标刻度设置
-    ax.xaxis.set_major_locator(MultipleLocator(10))
-    ax.yaxis.set_major_locator(MultipleLocator(10))
+     # 障碍坐标区域使用绿色表示
+    # 障碍横坐标x的范围
+    x_down_list = obstacle_coordinate_new[0]
+    x_up_list  = obstacle_coordinate_new[1]
+
+    # 障碍纵坐标y的范围
+    y_down_list = obstacle_coordinate_new[2]
+    y_up_list = obstacle_coordinate_new[3]
+    for j in range(0, obstacles_Num):
+        x_down_value= x_down_list[0][j]
+        x_up_value= x_up_list[0][j]
+        y_down_value= y_down_list[0][j]
+        y_up_value= y_up_list[0][j]
+       
+        plt.fill_between(range(x_down_value, x_up_value), y_down_value , y_up_value, facecolor='green')
+    ax.scatter((x_down_list[0][1]+x_up_list[0][1])/2, (
+             y_down_list[0][1]+y_up_list[0][1])/2, s = 100, color = 'green',label = 'Obstacle', marker = 's')
+    # 图片坐标刻度设置
+    # 2000*2000
+    # ax.xaxis.set_major_locator(MultipleLocator(100))
+    # ax.yaxis.set_major_locator(MultipleLocator(40))
+    ax.xaxis.set_major_locator(MultipleLocator(40))
+    ax.yaxis.set_major_locator(MultipleLocator(20))
     #设置x轴、y轴名称
     ax.set_xlabel('X(m)')
     ax.set_ylabel('Y(m)')
     
     print "x_list =", x_list
     print "y_list =", y_list
-    ax.scatter(x_list, y_list,color = 'red', marker = 'o')
+     # 每个节点用红圈圈表示出来
+    ax.scatter(x_list, y_list,color = 'r',label = 'Node', marker = 'o')
+    # ax.scatter(x_obstacle_list, y_obstacle_list,color = 'g', marker = '8')
     # S点为红色正方形，并且大一点
-    ax.scatter(x_list[0], y_list[0], s = 100, color = 'k', marker = 's')
- 
+    ax.scatter(x_list[0], y_list[0], s = 100, color = 'k',label = 'S', marker = 's')
+    
     ax.text(x_list[0], y_list[0], 'S', fontsize=20)
     for i in range(1, len(y_list)):
         ax.text(x_list[i], y_list[i], i, fontsize = 10)
@@ -145,6 +194,7 @@ def NodeToOtherNodeLink(x, y, label):
             x_temp = [x_list[i + 1], x_list[0]]
             y_temp = [y_list[i + 1], y_list[0]]
             plt.plot(x_temp, y_temp, label,linewidth=1)
+    ax.legend(loc='best', edgecolor='black')
     plt.xlim([0 - 1,edge_n + 1]) #设置绘图X边界                                                                                                   
     plt.ylim([0 - 1,edge_n + 1]) #设置绘图Y边界
     plt.show()
@@ -327,15 +377,86 @@ def ChangeChildrenTour(P_temp, N_distance_temp):
 
 # 程序从这里开始执行
 if __name__ == "__main__":
+    
+    # 随机生成障碍区域
+    for i in range(0, obstacles_Num):
+        x_down[0][i] = (random.randint(1, edge_n))
+        y_down[0][i] = (random.randint(1, edge_n))
+    print "x_down =", x_down
+    print "y_down =", y_down
+    # 保证障碍在要操作的二维区域内
+    for i in range(0, obstacles_Num):
+        if x_down[0][i] < edge_n - 10:
+            x_up[0][i] = x_down[0][i] + 10
+        else:
+            x_temp = x_down[0][i]
+            x_up[0][i] = x_temp
+            x_down[0][i] = x_temp - 10
+        if y_down[0][i] < edge_n - 10:
+            y_up[0][i] = y_down[0][i] + 10
+        else:
+            y_temp = y_down[0][i]
+            y_up[0][i] = y_temp
+            y_down[0][i] = y_temp - 10
+    print "x_down =", x_down
+    print "x_up =", x_up
+    print "y_down =", y_down
+    print "y_up =", y_up
+    obstacle_coordinate.append(x_down)
+    obstacle_coordinate.append(x_up)
+    obstacle_coordinate.append(y_down)
+    obstacle_coordinate.append(y_up)
+    print "obstacle_coordinate =", obstacle_coordinate
     # 随机在1000*1000的空间内生成N辆电单车的坐标
     # 将生成的电单车坐标分别保存到x,y列表中
     # 保存节点序号的
     N_i = []
     N_i.append(0)
     v = 5   # 初始化电单车的运行速度为 5m/s
+    # 保证电单车不在障碍区域内
     for i in range(1, N+1):
+        print "i =", i
         N_x[0][i] = round(random.uniform(1, edge_n), 2)
         N_y[0][i] = round(random.uniform(1, edge_n), 2)
+        print "第一层for循环"
+        print "N_x[0][", i, "] =", N_x[0][i]
+        print "N_y[0][", i, "] =", N_y[0][i] 
+        j = 0
+        while(j != obstacles_Num):
+            # print "初始化j =", j
+            j_temp = j
+            equal_flag = True
+            if((N_x[0][i] > x_down[0][j_temp]) and (N_x[0][i] < x_up[0][j_temp])) and(
+                    (N_y[0][i] > y_down[0][j_temp]) and (N_y[0][i] < y_up[0][j_temp])):
+                print "初始j_temp =", j_temp
+                j_temp = j_temp + 1
+                print "更新的j_temp =", j_temp
+                print "equal_flag =", equal_flag 
+                if equal_flag is True:
+                    print "表示有电单车在障碍区域里面了，不符合,更新当前电单车坐标"
+                    N_x[0][i] = round(random.uniform(1, edge_n), 2)
+                    N_y[0][i] = round(random.uniform(1, edge_n), 2)
+                    print "第二层for循环更新值"
+                    print "N_x[0][", i, "] =", N_x[0][i]
+                    print "N_y[0][", i, "] =", N_y[0][i]
+                    j_temp = 0
+                    j = 0
+                    print "更新j =", j
+                    print "更新j_temp =", j_temp
+                equal_flag = False
+                
+                
+                if j_temp >= obstacles_Num:
+                    j = j_temp 
+                    print "退出while（）"
+                    break
+            else:
+                j = j + 1
+                if j >= obstacles_Num:
+                    print "退出while（）"
+                    break
+            
+            
         # 每辆点电单车的电量，初始化为840Kj
         Es_i[0][i] = 840000
         # 电车运行方向的初始化
@@ -566,7 +687,7 @@ if __name__ == "__main__":
     PrintNewMatrix(N_distance, len(Node_optimal_sort))
     
     # 加入S点后，将电单车前后连接起来
-    NodeToOtherNodeLink(x, y, 'b')
+    NodeToOtherNodeLink(x, y, 'b', obstacle_coordinate)
    
     print "开始构造子回路"
     
--- 201810_201909_BeforeGraduateStudent/python_programming/Period_Charge/test.py
+++ 201810_201909_BeforeGraduateStudent/python_programming/Period_Charge/test.py
@@ -6,7 +6,7 @@ import time
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 # 数据初始化
-N = 20   # 假设我有N辆电单车
+N = 10   # 假设我有N辆电单车
 edge_n = 500 # 假设定义的二维空间范围是 edge_n * edge_n
 # 初始化电单车在二维空间中的坐标
 N_x = np.empty([1, N + 1], float)
@@ -28,6 +28,142 @@ x_up = np.empty([1, obstacles_Num], int)
 y_down = np.empty([1, obstacles_Num], int)
 y_up = np.empty([1, obstacles_Num], int)
 
+def AllNodeLink(x, y, obstacle_coordinate_new):
+    x_list = x
+    y_list = y
+    
+    # 调整生成的图片的大小
+    plt.rcParams['figure.figsize'] = (10.0, 10.0) # 设置figure_size尺寸
+    #创建图并命名
+    plt.figure('Scatter fig')
+    plt.title('S & Node')
+    ax = plt.gca()
+    
+    plt.grid()
+    # 障碍坐标区域使用绿色表示
+    # 障碍横坐标x的范围
+    x_down_list = obstacle_coordinate_new[0]
+    x_up_list  = obstacle_coordinate_new[1]
+
+    # 障碍纵坐标y的范围
+    y_down_list = obstacle_coordinate_new[2]
+    y_up_list = obstacle_coordinate_new[3]
+    for j in range(0, obstacles_Num):
+        x_down_value= x_down_list[0][j]
+        x_up_value= x_up_list[0][j]
+        y_down_value= y_down_list[0][j]
+        y_up_value= y_up_list[0][j]
+       
+        plt.fill_between(range(x_down_value, x_up_value), y_down_value , y_up_value, facecolor='green')
+    ax.scatter((x_down_list[0][1]+x_up_list[0][1])/2, (
+             y_down_list[0][1]+y_up_list[0][1])/2, s = 100, color = 'green',label = 'Obstacle', marker = 's')
+    # 图片坐标刻度设置
+    # 2000*2000
+    # ax.xaxis.set_major_locator(MultipleLocator(100))
+    # ax.yaxis.set_major_locator(MultipleLocator(40))
+    
+    ax.xaxis.set_major_locator(MultipleLocator(40))
+    ax.yaxis.set_major_locator(MultipleLocator(20))
+    #设置x轴、y轴名称
+    ax.set_xlabel('X(m)')
+    ax.set_ylabel('Y(m)')
+    
+    print "x_list =", x_list
+    print "y_list =", y_list
+    # 每个节点用红圈圈表示出来
+    ax.scatter(x_list, y_list,color = 'r',label = 'Node', marker = 'o')
+    # ax.scatter(x_obstacle_list, y_obstacle_list,color = 'g', marker = '8')
+    # S点为红色正方形，并且大一点
+    ax.scatter(x_list[0], y_list[0], s = 100, color = 'k',label = 'S', marker = 's')
+ 
+    ax.text(x_list[0], y_list[0], 'S', fontsize=20)
+    for i in range(1, len(y_list)):
+        ax.text(x_list[i], y_list[i], i, fontsize = 10)
+    # 将所有节点与充电桩S连起来
+    # for j in range(1, len(y_list)):
+        # 两两连接
+        # x_temp = [x_list[0], x_list[j]]
+        # y_temp = [y_list[0], y_list[j]]
+        # 调节途中线条颜色，粗细
+        # plt.plot(x_temp, y_temp, 'b',linewidth=0.5)
+    ax.legend(loc='best', edgecolor='black')
+    # plt.legend(loc='best',frameon=False) #去掉图例边框
+    # plt.legend(loc='best',edgecolor='blue') #设置图例边框颜色
+    # plt.legend(loc='best',facecolor='blue') #设置图例背景颜色,若无边框,参数无效
+    plt.xlim([0 - 1,edge_n + 1]) #设置绘图X边界                                                                                                   
+    plt.ylim([0 - 1,edge_n + 1]) #设置绘图Y边界
+    plt.show()
+    return
+
+# 将节点前后连接起来
+def NodeToOtherNodeLink(x, y, label, obstacle_coordinate_new):
+    print "x =", x
+    print "y =", y
+    #分别存放所有点的横坐标和纵坐标，一一对应
+    x_list = x
+    y_list = y
+    
+    #创建图并命名
+    plt.figure('Scatter fig')
+    plt.title('S & Node')
+    ax = plt.gca()
+    plt.grid()
+     # 障碍坐标区域使用绿色表示
+    # 障碍横坐标x的范围
+    x_down_list = obstacle_coordinate_new[0]
+    x_up_list  = obstacle_coordinate_new[1]
+
+    # 障碍纵坐标y的范围
+    y_down_list = obstacle_coordinate_new[2]
+    y_up_list = obstacle_coordinate_new[3]
+    for j in range(0, obstacles_Num):
+        x_down_value= x_down_list[0][j]
+        x_up_value= x_up_list[0][j]
+        y_down_value= y_down_list[0][j]
+        y_up_value= y_up_list[0][j]
+       
+        plt.fill_between(range(x_down_value, x_up_value), y_down_value , y_up_value, facecolor='green')
+    ax.scatter((x_down_list[0][1]+x_up_list[0][1])/2, (
+             y_down_list[0][1]+y_up_list[0][1])/2, s = 100, color = 'green',label = 'Obstacle', marker = 's')
+    # 图片坐标刻度设置
+    # 2000*2000
+    # ax.xaxis.set_major_locator(MultipleLocator(100))
+    # ax.yaxis.set_major_locator(MultipleLocator(40))
+    ax.xaxis.set_major_locator(MultipleLocator(40))
+    ax.yaxis.set_major_locator(MultipleLocator(20))
+    #设置x轴、y轴名称
+    ax.set_xlabel('X(m)')
+    ax.set_ylabel('Y(m)')
+    
+    print "x_list =", x_list
+    print "y_list =", y_list
+     # 每个节点用红圈圈表示出来
+    ax.scatter(x_list, y_list,color = 'r',label = 'Node', marker = 'o')
+    # ax.scatter(x_obstacle_list, y_obstacle_list,color = 'g', marker = '8')
+    # S点为红色正方形，并且大一点
+    ax.scatter(x_list[0], y_list[0], s = 100, color = 'k',label = 'S', marker = 's')
+    
+    ax.text(x_list[0], y_list[0], 'S', fontsize=20)
+    for i in range(1, len(y_list)):
+        ax.text(x_list[i], y_list[i], i, fontsize = 10)
+    # 将节点连接构成回路
+    for i in range(0, N):
+        # 前后连接
+        x_temp = [x_list[i], x_list[i+1]]
+        y_temp = [y_list[i], y_list[i+1]]
+        plt.plot(x_temp, y_temp, label,linewidth=1)
+        if i == N - 1:
+            print 'i =', i
+            x_temp = [x_list[i + 1], x_list[0]]
+            y_temp = [y_list[i + 1], y_list[0]]
+            plt.plot(x_temp, y_temp, label,linewidth=1)
+    ax.legend(loc='best', edgecolor='black')
+    plt.xlim([0 - 1,edge_n + 1]) #设置绘图X边界                                                                                                   
+    plt.ylim([0 - 1,edge_n + 1]) #设置绘图Y边界
+    plt.show()
+    return 
+
+# 随机生成障碍区域
 for i in range(0, obstacles_Num):
     x_down[0][i] = (random.randint(1, edge_n))
     y_down[0][i] = (random.randint(1, edge_n))
@@ -57,124 +193,54 @@ obstacle_coordinate.append(y_down)
 obstacle_coordinate.append(y_up)
 print "obstacle_coordinate =", obstacle_coordinate
 
-def AllNodeLink(x, y, obstacle_coordinate_new):
-    x_list = x
-    y_list = y
-    
-    # x_obstacle_list = obstacle_coordinate_new[0][0]
-    # y_obstacle_list = obstacle_coordinate_new[1][0]
-    # 调整生成的图片的大小
-    plt.rcParams['figure.figsize'] = (10.0, 10.0) # 设置figure_size尺寸
-    #创建图并命名
-    plt.figure('Scatter fig')
-    plt.title('S & Node')
-    ax = plt.gca()
-    
-    plt.grid()
-    # 障碍坐标区域使用绿色表示
-    # range(40, 81) 表示选中横坐标的范围
-    # 障碍横坐标x的范围
-    x_down_list = obstacle_coordinate_new[0]
-    x_up_list  = obstacle_coordinate_new[1]
-    # 障碍纵坐标y的范围
-    y_down_list = obstacle_coordinate_new[2]
-    y_up_list = obstacle_coordinate_new[3]
-    for j in range(0, obstacles_Num):
-        x_down_value= x_down_list[0][j]
-        x_up_value= x_up_list[0][j]
-        y_down_value= y_down_list[0][j]
-        y_up_value= y_up_list[0][j]
-        
-        plt.fill_between(range(x_down_value, x_up_value), y_down_value , y_up_value, facecolor='green')
-    # 图片坐标刻度设置
-    # 2000*2000
-    # ax.xaxis.set_major_locator(MultipleLocator(100))
-    # ax.yaxis.set_major_locator(MultipleLocator(40))
-    
-    ax.xaxis.set_major_locator(MultipleLocator(40))
-    ax.yaxis.set_major_locator(MultipleLocator(20))
-    #设置x轴、y轴名称
-    ax.set_xlabel('X(m)')
-    ax.set_ylabel('Y(m)')
-    
-    print "x_list =", x_list
-    print "y_list =", y_list
-    # 每个节点用红圈圈表示出来
-    ax.scatter(x_list, y_list,color = 'r', marker = 'o')
-    # ax.scatter(x_obstacle_list, y_obstacle_list,color = 'g', marker = '8')
-    # S点为红色正方形，并且大一点
-    ax.scatter(x_list[0], y_list[0], s = 100, color = 'k', marker = 's')
- 
-    ax.text(x_list[0], y_list[0], 'S', fontsize=20)
-    for i in range(1, len(y_list)):
-        ax.text(x_list[i], y_list[i], i, fontsize = 10)
-    # 将所有节点与充电桩S连起来
-    for j in range(1, len(y_list)):
-        # 两两连接
-        x_temp = [x_list[0], x_list[j]]
-        y_temp = [y_list[0], y_list[j]]
-        # 调节途中线条颜色，粗细
-        plt.plot(x_temp, y_temp, 'b',linewidth=0.5)
-            
-    plt.xlim([0 - 1,edge_n + 1]) #设置绘图X边界                                                                                                   
-    plt.ylim([0 - 1,edge_n + 1]) #设置绘图Y边界
-    plt.show()
-    return
-
-# 将节点前后连接起来
-def NodeToOtherNodeLink(x, y, label):
-    print "x =", x
-    print "y =", y
-    #分别存放所有点的横坐标和纵坐标，一一对应
-    x_list = x
-    y_list = y
-    
-    #创建图并命名
-    plt.figure('Scatter fig')
-    plt.title('S & Node')
-    ax = plt.gca()
-    plt.grid()
-    # 图片坐标刻度设置
-    # 2000*2000
-    # ax.xaxis.set_major_locator(MultipleLocator(100))
-    # ax.yaxis.set_major_locator(MultipleLocator(40))
-    ax.xaxis.set_major_locator(MultipleLocator(40))
-    ax.yaxis.set_major_locator(MultipleLocator(20))
-    #设置x轴、y轴名称
-    ax.set_xlabel('X(m)')
-    ax.set_ylabel('Y(m)')
-    
-    print "x_list =", x_list
-    print "y_list =", y_list
-    ax.scatter(x_list, y_list,color = 'red', marker = 'o')
-    # S点为红色正方形，并且大一点
-    ax.scatter(x_list[0], y_list[0], s = 100, color = 'k', marker = 's')
- 
-    ax.text(x_list[0], y_list[0], 'S', fontsize=20)
-    for i in range(1, len(y_list)):
-        ax.text(x_list[i], y_list[i], i, fontsize = 10)
-    # 将节点连接构成回路
-    for i in range(0, N):
-        # 前后连接
-        x_temp = [x_list[i], x_list[i+1]]
-        y_temp = [y_list[i], y_list[i+1]]
-        plt.plot(x_temp, y_temp, label,linewidth=1)
-        if i == N - 1:
-            print 'i =', i
-            x_temp = [x_list[i + 1], x_list[0]]
-            y_temp = [y_list[i + 1], y_list[0]]
-            plt.plot(x_temp, y_temp, label,linewidth=1)
-    plt.xlim([0 - 1,edge_n + 1]) #设置绘图X边界                                                                                                   
-    plt.ylim([0 - 1,edge_n + 1]) #设置绘图Y边界
-    plt.show()
-    return 
-
 N_i = []
 N_i.append(0)
 v = 5   # 初始化电单车的运行速度为 5m/s
+
+# 保证电单车不在障碍区域内
 for i in range(1, N+1):
+    print "i =", i
     N_x[0][i] = round(random.uniform(1, edge_n), 2)
     N_y[0][i] = round(random.uniform(1, edge_n), 2)
+    print "第一层for循环"
+    print "N_x[0][", i, "] =", N_x[0][i]
+    print "N_y[0][", i, "] =", N_y[0][i] 
+    j = 0
+    while(j != obstacles_Num):
+        # print "初始化j =", j
+        j_temp = j
+        equal_flag = True
+        if((N_x[0][i] > x_down[0][j_temp]) and (N_x[0][i] < x_up[0][j_temp])) and(
+                (N_y[0][i] > y_down[0][j_temp]) and (N_y[0][i] < y_up[0][j_temp])):
+            print "初始j_temp =", j_temp
+            j_temp = j_temp + 1
+            print "更新的j_temp =", j_temp
+            print "equal_flag =", equal_flag 
+            if equal_flag is True:
+                print "表示有电单车在障碍区域里面了，不符合,更新当前电单车坐标"
+                N_x[0][i] = round(random.uniform(1, edge_n), 2)
+                N_y[0][i] = round(random.uniform(1, edge_n), 2)
+                print "第二层for循环更新值"
+                print "N_x[0][", i, "] =", N_x[0][i]
+                print "N_y[0][", i, "] =", N_y[0][i]
+                j_temp = 0
+                j = 0
+                print "更新j =", j
+                print "更新j_temp =", j_temp
+            equal_flag = False
+            
+            
+            if j_temp >= obstacles_Num:
+                j = j_temp 
+                print "退出while（）"
+                break
+        else:
+            j = j + 1
+            if j >= obstacles_Num:
+                print "退出while（）"
+                break
+        
+        
     # 每辆点电单车的电量，初始化为840Kj
     Es_i[0][i] = 840000
     # 电车运行方向的初始化
@@ -212,15 +278,15 @@ print "剩余能量最少的节点是：", P_i_Max_Node
 # S表示充电桩的位置，它的开始时坐标的确定，主要是，靠近剩余能量最小的节点或者说功率最大的点
 S = []
 # S 的坐标比较讲究，不能部署到边界上
-if N_x_new[0][P_i_Max_Node] < edge_n - 20:
-    S_x = N_x_new[0][P_i_Max_Node] + 20
+if N_x_new[0][P_i_Max_Node] < edge_n - 1:
+    S_x = N_x_new[0][P_i_Max_Node] + 1
 else:
-    S_x = N_x_new[0][P_i_Max_Node] - 20
+    S_x = N_x_new[0][P_i_Max_Node] - 1
     
-if N_y_new[0][P_i_Max_Node] < edge_n - 20:
-    S_y = N_y_new[0][P_i_Max_Node] + 20
+if N_y_new[0][P_i_Max_Node] < edge_n - 1:
+    S_y = N_y_new[0][P_i_Max_Node] + 1
 else:
-    S_y = N_y_new[0][P_i_Max_Node] - 20
+    S_y = N_y_new[0][P_i_Max_Node] - 1
 S.append(S_x)
 S.append(S_y)
 print "充电桩坐标为：", (S[0], S[1])
@@ -237,4 +303,4 @@ print "y =", y
 # 将电单车之间两两连接起来画图
 AllNodeLink(x, y,obstacle_coordinate)
 # 将哈密顿回路连接起来
-NodeToOtherNodeLink(x, y, 'g')
+NodeToOtherNodeLink(x, y, 'g', obstacle_coordinate)
